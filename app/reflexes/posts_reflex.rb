087a25032f358f81d0f25bb9d017d1a1b5cee4ea
Tony Blum <tonyblum@me.com>
2020-12-05 09:35:59 -0800

    cable_ready broadcasts for autoincrement, likes/reposts

--- app/reflexes/posts_reflex.rb
+++ app/reflexes/posts_reflex.rb
@@ -5,11 +5,21 @@ class PostsReflex < ApplicationReflex
   def repost
     post = Post.find(element.dataset[:id])
     post.increment! :reposts_count
+    cable_ready["timeline"].text_content(
+      selector: "#post-#{post.id}-reposts",
+      text: post.reposts_count
+    )
+    cable_ready.broadcast
   end
 
   def like
     post = Post.find(element.dataset[:id])
     post.increment! :likes_count
+    cable_ready["timeline"].text_content(
+      selector: "#post-#{post.id}-likes",
+      text: post.likes_count
+    )
+    cable_ready.broadcast
   end
 
 end
